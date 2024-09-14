34066503a6fa14c4de8a811351cbb029964d8c06
Me1k0r <runner-me1k0r@proton.me>
2024-09-14 15:07:42 +0200

    edit: color - prototype function to create a new rgb color

--- src/color.h
+++ src/color.h
@@ -32,11 +32,18 @@ typedef struct hsl
  * @brief Creates a new hex color based on the given string
  * 
  * @param hexStr {const unsigned char} : hex falue with or without "#"
- * @return hex* :ew hex color
+ * @return hex* {hex} : new hex color
  */
 hex *newHexColor(const unsigned char *hexStr);
 
-
-
+/**
+ * @brief Creates a new RGB color based on the given red, green and blue values
+ * 
+ * @param r {const unsigned char} : red value
+ * @param g {const unsigned char} : green value
+ * @param b {const unsigned char} : blue value
+ * @return rgb* {rgb} : new rgb color
+ */
+rgb *newRGBColor(const unsigned char r, const unsigned char g, const unsigned char b);
 
 #endif
