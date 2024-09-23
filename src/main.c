8b62f0f6cbcb1ec373f677d6f487308792b1dba6
Jaufré Lallement <runner-me1k0r@proton.me>
2024-09-23 10:40:07 +0200

    edit: calling deleteColorDList to free mem

--- src/main.c
+++ src/main.c
@@ -1,105 +1,95 @@
 #include <getopt.h>
+#include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <regex.h>
 
-#include "utils.h"
 #include "color.h"
 #include "color_dlist.h"
+#include "utils.h"
 
 static int verbose_flag = 0;
 
-int main(int argc, char *argv[])
-{
-	int opt;
-	int optionIndex = 0;
-	
-	static struct option longOptions[] = 
-	{
-		{ "color", required_argument, 0, 'c' },
-		{ "saturate", required_argument, 0, 's' },
-		{ "distance", required_argument, 0, 'd' },
-		{ "verbose", no_argument, &verbose_flag, 'v' },
-		{ "help", no_argument, 0, 'H' },
-	};
+int main(int argc, char *argv[]) {
+  int opt;
+  int optionIndex = 0;
 
-	int colorCount = 0;
-	ColorDList *colors = newColorDList();
-	float saturation;
+  static struct option longOptions[] = {
+      {"color", required_argument, 0, 'c'},
+      {"saturate", required_argument, 0, 's'},
+      {"distance", required_argument, 0, 'd'},
+      {"verbose", no_argument, &verbose_flag, 'v'},
+      {"help", no_argument, 0, 'H'},
+  };
 
-	enum {
-		SATURATE,
-		HEX2RGB,
-		RGB2HEX,
-		DISTANCE
-	} mode;
+  int colorCount = 0;
+  ColorDList *colors = newColorDList();
+  float saturation;
 
-	while ((
-		opt = getopt_long(argc, argv, "c:s:d:vH", longOptions, &optionIndex
-	)) != -1) 
-	{
-		switch (opt)
-		{
-			case 0:
-				if (longOptions[optionIndex].flag != 0)
-					break;
+  enum { SATURATE, HEX2RGB, RGB2HEX, DISTANCE } mode;
 
-				printf ("Option %s", longOptions[optionIndex].name);
+  while ((opt = getopt_long(argc, argv, "c:s:d:vH", longOptions,
+                            &optionIndex)) != -1) {
+    switch (opt) {
+    case 0:
+      if (longOptions[optionIndex].flag != 0)
+        break;
 
-				if (optarg)
-					printf (" with arg %s", optarg);
+      printf("Option %s", longOptions[optionIndex].name);
 
-				printf ("\n");
-				break;
+      if (optarg)
+        printf(" with arg %s", optarg);
 
-			case 'd':
-				printf ("option -d with value `%s'\n", optarg);
-				break;
-				
-			case 'c':
-				// New color
-				color *c = newColorFromStr(optarg);
-				colors = pushBackColorDList(colors, c);
-				
-				printColor(c);
-				break;
+      printf("\n");
+      break;
 
-			case 'H':
-				break;
+    case 'd':
+      printf("option -d with value `%s'\n", optarg);
+      break;
 
-			case 's':
-				saturation = atof(optarg);
-				printf("saturation: %f", saturation);
-				break;
+    case 'c':
+      // New color
+      color *c = newColorFromStr(optarg);
+      colors = pushBackColorDList(colors, c);
 
-			default:
-				abort();
-		}
-	}
+      printColor(c);
+      break;
 
-	if (verbose_flag)
+    case 'H':
+      break;
+
+    case 's':
+      saturation = atof(optarg);
+      printf("saturation: %f", saturation);
+      break;
+
+    default:
+      abort();
+    }
+  }
+
+  if (verbose_flag)
     puts("verbose flag is set");
 
-  if (optind < argc)
-	{
-		printf("non-option ARGV-elements: ");
-		while (optind < argc)
-			printf("%s ", argv[optind++]);
+  if (optind < argc) {
+    printf("non-option ARGV-elements: ");
+    while (optind < argc)
+      printf("%s ", argv[optind++]);
 
-		putchar('\n');
-	}
+    putchar('\n');
+  }
 
-	// printf("%s", colors);
+  // printf("%s", colors);
 
-	// const unsigned char hexCode[] = "#0afea4";
-	// hex *myColor = newHexColor(hexCode);
-	
-	// const unsigned char hexCode1[] = "#A36E65";
-	// hex *myColor1 = newHexColor(hexCode1);
+  // const unsigned char hexCode[] = "#0afea4";
+  // hex *myColor = newHexColor(hexCode);
 
-	// float colDistance = getHexBasicColorDistance(myColor, setHexColorSaturation(myColor1, 1));
-	// printf("Distance: %f", colDistance);
+  // const unsigned char hexCode1[] = "#A36E65";
+  // hex *myColor1 = newHexColor(hexCode1);
 
-	return 0;
+  // float colDistance = getHexBasicColorDistance(myColor,
+  // setHexColorSaturation(myColor1, 1)); printf("Distance: %f", colDistance);
+
+  deleteColorDList(colors);
+  return 0;
 }
