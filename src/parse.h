0c29204a9de5ee6d65f718fdbf42d5e54f3d3664
Me1k0r <runner-me1k0r@proton.me>
2024-10-28 18:30:58 +0100

    edit: parse - command unique names + longOptions

--- src/parse.h
+++ src/parse.h
@@ -1,4 +1,27 @@
 #ifndef __PARSE__H__
 #define __PARSE__H__
 
+#include <getopt.h>
+
+typedef enum
+{
+  CMD_HELP,
+  CMD_INFO,
+  CMD_DISTANCE,
+  CMD_SATURATE
+} commandmode;
+
+union commanddata
+{
+  float sat;
+};
+
+const struct option longOptions[] = {
+  {"color", required_argument, 0, 'c'},
+  {"distance", no_argument, 0, 'd'},
+  {"help", no_argument, 0, 'H'},
+  {"info", optional_argument, 0, 'i'},
+  {"saturate", required_argument, 0, 's'},
+};
+
 #endif
